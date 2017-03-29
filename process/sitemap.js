d1a394237c58beafc172aab732ce789e0c5a9721
wonknu10 <fabrice.labbe@adfab.fr>
2017-03-29 09:56:17 +0200

    rm log

--- process/sitemap.js
+++ process/sitemap.js
@@ -85,7 +85,6 @@ if(typeof pConfig.ABE_WEBSITE !== 'undefined' && pConfig.ABE_WEBSITE !== null) {
 				var regex = new RegExp(abe.config.sitemap.folders.regex)
 				Array.prototype.forEach.call(list, function(file) {
 					if (file.publish) {
-						console.log(file.publish)
 						var match = regex.exec(file.publish.html)
 						if(typeof match !== 'undefined' && match !== null
 							&& typeof match[1] !== 'undefined' && match[1] !== null) {
