b3dd99f6eea87ceb589a263531f802b17092d24c
wonknu10 <fabrice.labbe@adfab.fr>
2018-08-06 14:51:56 -0400

    v2.13

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -1,13 +1,11 @@
 'use strict';
 
 var hooks = {
-  afterSave: function(obj, abe) {
-  	if (!obj.publishAll && obj.type === "publish") {
-	    if (!abe.abeExtend.process('sitemap')) {
-	    	console.log('cannot run process sitemap, because an other one is already running')
-	    }
-  	}
-    return obj
+  afterPublish: function(result, filePath, abe) {
+    if (!abe.abeExtend.process('sitemap')) {
+      console.log('cannot run process sitemap, because an other one is already running')
+    }
+    return result;
   }
 };
 
